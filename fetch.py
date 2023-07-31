1dab17d37d404d8d5cf644f47cf8728a270a5b25
Niyas <nozelle@hotmail.com>
2023-07-31 14:00:34 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,55 +1,18 @@
 import streamlit as st
-import tweepy
+
 import requests
-from io import BytesIO
-from PIL import Image
 
-# Twitter API credentials
-consumer_key = 'YOUR_CONSUMER_KEY'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
-
-# Twitter authentication and API setup
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-# Function to extract image and tweet text from the Twitter link
-def extract_tweet_info(tweet_url):
-    tweet_id = tweet_url.split('/')[-1]
-    try:
-        tweet = api.get_status(tweet_id, tweet_mode='extended')
-        tweet_text = tweet.full_text
-        if 'media' in tweet.entities:
-            media_url = tweet.entities['media'][0]['media_url_https']
-            image_response = requests.get(media_url)
-            image = Image.open(BytesIO(image_response.content))
-            return tweet_text, image
-        else:
-            return tweet_text, None
-    except tweepy.TweepError as e:
-        st.error(f"Error: {e}")
-        return None, None
-
-# Streamlit app
-def main():
-    st.title("Twitter Image and Tweet Text Extractor")
-    st.write("Enter a Twitter link below to extract the image and tweet text.")
-
-    tweet_url = st.text_input("Twitter URL", "")
-
-    if st.button("Extract"):
-        if tweet_url:
-            tweet_text, image = extract_tweet_info(tweet_url)
-            if tweet_text and image:
-                st.image(image, caption=tweet_text, use_column_width=True)
-            elif tweet_text:
-                st.write(tweet_text)
-            else:
-                st.error("Failed to extract tweet information. Please check the URL.")
-        else:
-            st.warning("Please enter a Twitter URL.")
+def get_media_from_tweet_url(tweet_url):
+    response = requests.get(tweet_url)
+    if response.status_code == 200:
+        media_url = response.json()["entities"]["media"][0]["media_url"]
+        return media_url
+    else:
+        return None
 
 if __name__ == "__main__":
-    main()
+    tweet_url = "https://twitter.com/elonmusk/status/1546850431785919424"
+    media_url = get_media_from_tweet_url(tweet_url)
+    st.title("Twitter Image and Tweet Text Extractor")
+    st.image(media_url)
+
