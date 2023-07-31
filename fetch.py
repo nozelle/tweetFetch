7d4a00d0849bee269271080de2666c6c0ad2cc9d
Niyas <nozelle@hotmail.com>
2023-07-31 13:28:49 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,48 +1,51 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-from PIL import Image
 
-# Function to extract image and tweet text from the Twitter link using web scraping
-def extract_tweet_info(tweet_url):
-    try:
-        response = requests.get(tweet_url)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            tweet_text = soup.find('div', {'class': 'tweet-text'}).text.strip()
-            media_element = soup.find('div', {'class': 'AdaptiveMedia-photoContainer'})
-            if media_element:
-                media_url = media_element.find('img')['src']
-                image_response = requests.get(media_url)
-                image = Image.open(BytesIO(image_response.content))
-                return tweet_text, image
-            else:
-                return tweet_text, None
-        else:
-            st.error("Failed to fetch tweet. Please check the URL.")
-            return None, None
-    except requests.exceptions.RequestException as e:
-        st.error(f"Error: {e}")
-        return None, None
-
-# Streamlit app
 def main():
-    st.title("Twitter Image and Tweet Text Extractor")
-    st.write("Enter a Twitter link below to extract the image and tweet text.")
+    st.title("Malama Carbon Footprint Calculator")
 
-    tweet_url = st.text_input("Twitter URL", "")
+    # Initialize variables to store user inputs
+    energy_consumption = 0
+    transportation_emissions = 0
+    diet_emissions = 0
+    waste_production = 0
+    shopping_habits = 0
 
-    if st.button("Extract"):
-        if tweet_url:
-            tweet_text, image = extract_tweet_info(tweet_url)
-            if tweet_text and image:
-                st.image(image, caption=tweet_text, use_column_width=True)
-            elif tweet_text:
-                st.write(tweet_text)
-            else:
-                st.error("Failed to extract tweet information. Please check the URL.")
-        else:
-            st.warning("Please enter a Twitter URL.")
+    st.write("Please answer the following questions to calculate your carbon footprint:")
 
+    # Question 1: Energy consumption
+    energy_consumption = st.slider("1. Energy Consumption (kWh per month)", min_value=0, max_value=1000, step=10)
+
+    # Question 2: Transportation
+    transportation_emissions = st.slider("2. Transportation Emissions (kg CO2 per year)", min_value=0, max_value=10000, step=100)
+
+    # Question 3: Diet
+    diet_options = ["Plant-based", "Mixed (Some meat and dairy)", "High meat and dairy"]
+    diet_emissions = st.selectbox("3. Diet", diet_options)
+
+    # Question 4: Waste production
+    waste_production = st.slider("4. Waste Production (kg per week)", min_value=0, max_value=100, step=1)
+
+    # Question 5: Shopping habits
+    shopping_options = ["Minimal waste (buying local, reducing packaging, etc.)", "Regular shopping habits"]
+    shopping_habits = st.selectbox("5. Shopping Habits", shopping_options)
+
+    # Calculate total carbon footprint based on the user's inputs
+    total_carbon_footprint = energy_consumption * 0.35 + transportation_emissions * 0.15
+
+    if diet_emissions == "Plant-based":
+        total_carbon_footprint += 150
+    elif diet_emissions == "Mixed (Some meat and dairy)":
+        total_carbon_footprint += 400
+    elif diet_emissions == "High meat and dairy":
+        total_carbon_footprint += 800
+
+    total_carbon_footprint += waste_production * 0.1
+
+    st.markdown("<h2>Your estimated carbon footprint is:</h2>", unsafe_allow_html=True)
+
+    # Display the total_carbon_footprint value in a card format
+    total_carbon_footprint = int(total_carbon_footprint)
+    st.info(f"{total_carbon_footprint} kg CO2 per year")
+    
 if __name__ == "__main__":
     main()
